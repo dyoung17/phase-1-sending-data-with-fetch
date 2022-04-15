6db4f56aec0de5f6143299443fef74593ed40381
Daven Young <davenyoung@msn.com>
2022-04-15 15:37:18 -0600

    commit tree faaf0a0b84bfaec99edf0fa9e70e99a24f8059d9

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your code here
+function submitData(data){
+    fetch('http://localhost:3000/dogs',{
+    method: 'POST',
+    headers: {
+        'Content-Type' : 'application/JSON'
+    },
+    body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
